bf620cc3bc94d486fbfc7768755aa026385d2438
Zaw Win Tin <zawwintin21@gmail.com>
2022-03-05 18:01:59 +0630

    Initial Implementation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # laravel_deeper_training_project
+
+### Description
+ This is a Laravel Project trained by my Senior [Thet Min Htun](https://github.com/thetminnhtun-scm).
+### Installation
+```
+git pull https://github.com/Shadow-Beast/laravel_deeper_training_project.git
+composer install
+npm install
+npm run watch
+```
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="{{ asset('images/logo.svg') }}">
+    <link rel="icon" href="{{ asset('images/logo.svg') }}" type="image/png">
+    <meta name="theme-color" content="#00C292">
+
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    @yield('styles')
+</head>
+
+<body>
+    <div>
+        @include('layouts.header')
+
+        <!-- Page Content -->
+        <main class="min-h-screen">
+            {{ $slot }}
+        </main>
+
+        @include('layouts.footer')
+    </div>
+    <!-- Scripts -->
+
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    @yield('scripts')
+</body>
+
+</html>
