1de12d7a34957839f4673ad0e44557a2b6b16105
Zaw Win Tin <zawwintin21@gmail.com>
2022-03-12 00:43:39 +0630

    Adjust Loading Screen

--- resources/views/components/loading.blade.php
+++ resources/views/components/loading.blade.php
@@ -1,3 +1,3 @@
-<div id="loading" class="flex items-center w-full justify-center h-full bg-secondary z-40 fixed">
+<div id="loading" class="flex items-center justify-center w-full h-full bg-secondary z-40 fixed top-0 left-0">
     <img src="{{ asset('images/loading.gif') }}" class="pointer-events-none w-44 h-44" alt="Loading">
 </div>
